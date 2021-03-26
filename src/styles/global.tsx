93db9080924a12d357ca9a613b90aa3a87a15bbc
MarcosNSouza87 <31523870+MarcosNSouza87@users.noreply.github.com>
2021-03-26 07:45:35 -0300

    Update global.tsx
    edição global

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -1,4 +1,4 @@
-import { createGlobalStyle} from 'styled-components'
+import styled, { createGlobalStyle} from 'styled-components'
 
 export default createGlobalStyle`
     *{
@@ -13,4 +13,52 @@ export default createGlobalStyle`
         background:#3289e0;
     }
 
+`;
+
+
+export const Container = styled.div`
+    height: 100%;
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    align-items:center;
+`
+
+export const Header = styled.header`
+    display:flex;
+    background:#2c2c2c;
+    color:#fff;
+    width:100%;
+    align-items:center;
+    padding:20px;
+    justify-content:center;
+    font-size:1.7rem;
+`;
+
+export const Main = styled.main`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    align-items: center;
+`;
+
+
+export const Button = styled.button`
+    background-color:#3289e0;
+    color:#fff;
+    border:none;
+    border-radius:30px;
+    height: 40px;
+    width:40px;
+    align-items:center;
+    text-align: center;
+    justify-content: center;
+    display: flex;
+    font-size:1rem;
+    animation-delay:2s;
+    :hover{
+        cursor:pointer;
+        background-color:#fff;
+        color:#3289e0;
+    }
 `;
