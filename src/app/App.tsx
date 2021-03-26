d742fc2f26030f8c541a22288b8cb32d9bb920d2
MarcosNSouza87 <31523870+MarcosNSouza87@users.noreply.github.com>
2021-03-26 08:56:40 -0300

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,49 +1,24 @@
-import React, { ChangeEvent, FormEvent, useState } from 'react';
-
-import GlobalStyle from '../styles/global'
-
-import { Button, Container,Form,Header, Main } from '../styles/components/stylesPre'
+import React, {  memo, useEffect } from 'react';
+import GlobalStyle,{  Container,Header, Main } from '../styles/global'
 import ListToDo from 'components/ListToDo';
-import { RootStateOrAny, useSelector } from 'react-redux';
-
-interface Todo {
-  id:number
-  title:string
-  description:string
-}
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from 'store';
+import { deleteTodo, getAllTodos } from 'store/ducks/todo.duck';
+import FormTodo from 'components/Form';
 
 function App() {
 
-  const [entity,setEntity] = useState<Todo>({id:0,title:'',description:''})
+  const dispatch = useDispatch();
+  const data = useSelector((store:RootState)=> store.todoReducer)
 
-  const msg = useSelector((state:RootStateOrAny)=> state.exampleReducer)
-  console.log(msg);
+  useEffect(() => {
+    dispatch(getAllTodos());
+  }, [dispatch]);
 
-  const items = 
-  [
-    {  id:1,title:"t1",description:"t1 is ffas"   },
-    {  id:2,title:"t2",description:"t2 is ffas"   },
-  ]
 
   const handleRemoveItem = (id:number) => {
-    alert(id);
+    dispatch(deleteTodo(id));
   }
-
-
-
-  const handleAddNewTodo = (event:FormEvent) => {
-    event.preventDefault();
-    console.log(entity);
-  }
-
-
-  const handleInputChange = (event:ChangeEvent<HTMLInputElement>) => {
-    const {name,value} = event.target
-    if(entity !== undefined){
-      setEntity({...entity,[name]:value});
-      console.log('tst');
-    }
-}
 
   return (
       <Container>
@@ -52,18 +27,17 @@ function App() {
           To do List Challenger 
         </Header>
         <Main>
-          <Form onSubmit={handleAddNewTodo}>
-            <input name="title" placeholder="Título" value={entity?.title} onChange={handleInputChange}/>
-            <input name="description" placeholder="Descrição" value={entity?.description} onChange={handleInputChange}/>        
-            <Button type="submit">Adicionar</Button>
-          </Form>
-          <ListToDo
-            Lista={items}
-            handleRemoveItem={handleRemoveItem}
-          />
+          <FormTodo/>
+          {
+            data.data.length > 0 &&
+            <ListToDo
+              Lista={data.data}
+              handleRemoveItem={handleRemoveItem}
+            />
+          }
         </Main>
       </Container>
   );
 }
 
-export default App;
+export default memo(App);
